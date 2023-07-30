0ecb37b3cec0780f4f0f63e4b15287b436f5dd80
benhga <benh@genesis-analytics.com>
2023-07-30 10:43:06 +0100

    dashboard implemented

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc,  Output, Input,dash_table
 import plotly.express as px
 
 app = Dash(__name__)
@@ -8,18 +8,58 @@ df = pd.read_csv("./data/preprocessed.csv")
 
 fig = px.line(df, 'date', 'sales')
 
-app.layout = html.Div(children=[
-    html.H1(children='Date vs Sales'),
+app.layout = html.Div(
+    style={'font-family': 'Arial, sans-serif', 'margin': '20px'},
+    children=[
+        html.H1(
+            children='Date vs Sales',
+            style={'text-align': 'center', 'margin-bottom': '20px'}
+        ),
 
-    html.Div(children='''
-        Graphing sales of pink morsels against the date
-    '''),
+        html.Div(
+            children=[
+                dcc.Graph(
+                    id='graph',
+                    figure=px.line(df, 'date', 'sales')
+                )
+            ],
+            style={'margin-bottom': '30px'}
+        ),
 
-    dcc.Graph(
-        id='graph',
-        figure=fig
-    )
-])
+        html.Div(
+            children=[
+                html.P('Choose which region you would like to view:', style={'margin-bottom': '10px'}),
+                dcc.RadioItems(
+                    id='radio-buttons',
+                    options=[
+                        {'label': 'North', 'value': 'north'},
+                        {'label': 'East', 'value': 'east'},
+                        {'label': 'South', 'value': 'south'},
+                        {'label': 'West', 'value': 'west'},
+                        {'label': "All", 'value': "None"}
+                    ],
+                    value=None,
+                    labelStyle={'display': 'block', 'margin-right': '10px'}
+                )
+            ],
+            style={'margin-bottom': '30px'}
+        )
+    ]
+)
+
+@app.callback(
+    Output('graph', 'figure'),
+    [Input('radio-buttons', 'value')]
+)
+
+def update_graph(selected_region):
+    if selected_region == 'None':
+        fig = px.line(df, 'date', 'sales')
+    else:
+        filtered_df = df[df['region']==selected_region]
+        fig = px.line(filtered_df, 'date', 'sales')
+
+    return fig
 
 if __name__ == '__main__':
     app.run(debug=True)
